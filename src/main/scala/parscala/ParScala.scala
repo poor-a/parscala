7442d35529ea3742da654b84d884e14e2b071c30
Artúr Poór <gombocarti@gmail.com>
2017-06-03 21:39:54 +0200

    Add version of astOfClass. Fix indentation.

--- src/main/scala/parscala/ParScala.scala
+++ src/main/scala/parscala/ParScala.scala
@@ -19,35 +19,41 @@ object ParScala {
     val objectName : TermName = compiler.freshTermName("o")(freshGen)
     val funName : TermName = compiler.freshTermName("f")(freshGen)
     val code : String = "package %s { object %s { def %s : Any = { %s } } }".format( packageName
-                                                                               , objectName
-                                                                               , funName
-                                                                               , expr
-                                                                               )
+                                                                                   , objectName
+                                                                                   , funName
+                                                                                   , expr
+                                                                                   )
     val source : SourceFile = compiler.newSourceFile(code)
     val r : compiler.Run = new compiler.Run
     r.compileSources(List(source))
     val units : Iterator[CompilationUnit] = r.units
     if (units.nonEmpty) {
-	val q"package $_ { object $_ { def $_(...$_) : $_ = $body } }" = units.next().body
-	Some((body, source))
-    } else
-	None
+      val q"package $_ { object $_ { def $_(...$_) : $_ = $body } }" = units.next().body
+      Some((body, source))
+    } else {
+      None
+    }
   }
 
   def astOfExpr : String => Option[Tree] = (astOfExprWithSource _) andThen (_.map(_._1))
 
-  def astOfClass(cls : String) : Tree = {
+  def astOfClassWithSource(cls : String) : Option[(Tree, SourceFile)] = {
     import compiler.Quasiquote
 
     val freshGen = compiler.currentFreshNameCreator
     val packageName : TermName = compiler.freshTermName("p")(freshGen)
-    val source : String = "package %s { %s }".format( packageName
-                                                    , cls
-                                                    )
-    val dummyUnit : CompilationUnit = compiler.newCompilationUnit(source)
+    val code : String = "package %s { %s }".format(packageName, cls)
+    val source : SourceFile = compiler.newSourceFile(code)
     val r : compiler.Run = new compiler.Run
-    r.compileUnits(List(dummyUnit), r.parserPhase)
-    val q"package $_ { $clsAst }" = dummyUnit.body
-    clsAst
+    r.compileSources(List(source))
+    val units : Iterator[CompilationUnit] = r.units
+    if (units.nonEmpty) {
+      val q"package $_ { $clsAst }" = units.next().body
+      Some((clsAst, source))
+    } else {
+      None
+    }
   }
+
+  def astOfClass : String => Option[Tree] = (astOfClassWithSource _) andThen (_.map(_._1))
 }
